cc1021e4c990924c34e26240640c3f6d0f44a47a
Carpe-liam <alexander.liam.tyler@gmail.com>
2021-10-25 11:33:42 -0700

    a

---
+++ introduction/destructuring.js
@@ -0,0 +1,47 @@
+// PROB 1 ===============
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+/*
+    randomCar will be cars[0]
+    ',' skips 1 index value [0]->[1]
+    ,otherRandomCar will be cars[1]
+*/
+
+// PROB 2 ===============
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+/*
+
+*/
+
+
+// PROB 3 ===============
+
+/*
+
+*/
+
+// PROB 4 ===============
+
+/*
+
+*/
+
+// PROB 5 ===============
+
+/*
+
+*/
