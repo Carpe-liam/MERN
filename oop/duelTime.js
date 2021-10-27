80ddc9fd054bf51d9fa81ea1fd366261375086dd
Alex Tyler <alexander.liam.tyler@gmail.com>
2021-10-27 09:42:48 -0700

    W1D3-am

--- oop/duelTime.js
+++ oop/duelTime.js
@@ -14,6 +14,9 @@ class Unit extends Card {
 
     attack(target) {
         target.resilience -= this.power;
+        console.log("==========================================")
+        console.log(this.name + " attacks for " + this.power + " damage.")
+        console.log("==========================================")
     }
 }
 
@@ -27,10 +30,7 @@ class Effect extends Card {
 
     play(target) {
         if(target instanceof Unit){
-            let targetStat = target.stat
-            console.log(targetStat)
-            let effect = this.magnitude
-            console.log(effect)
+            target[this.stat] += this.magnitude
         } else {
             throw new Error ("Target Must Be Unit!")
         }
@@ -38,13 +38,32 @@ class Effect extends Card {
 
 }
 
+// ==== Units =====
 const redNinja = new Unit("Red Belt Ninja", 3, 3, 4)
 const blackNinja = new Unit("Black Belt Ninja", 4, 5, 4)
-const hardAlgo = new Effect("Hard Algorithm", 2, "Increase target resilience by 3", resilience, -3)
 
+// ==== Effects =====
+const hardAlgo = new Effect("Hard Algorithm", 2, "Increase target resilience by 3", "resilience", 3)
+const promiseReject = new Effect("Unhandled Promise Rejection", 1, "Reduce target resilience by 2", "resilience", -2)
+const pairProgramming = new Effect("Pair Programming", 3, "Increases targets power by 2", "power", 2)
+
+// turn 1
 console.log(redNinja)
 console.log(hardAlgo)
 hardAlgo.play(redNinja)
+console.log(redNinja)
+
+console.log("++++++++++++++++++++++++++++++++++")
+// turn 2
+console.log(blackNinja)
+console.log(promiseReject)
+promiseReject.play(redNinja)
+console.log(redNinja)
+
+// turn 3
+pairProgramming.play(redNinja)
+redNinja.attack(blackNinja)
+console.log(redNinja)
+console.log(blackNinja)
 
 
-// console.log(blackNinja)
